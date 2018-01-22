de60ff448836bf26c93c4375ea05580739586892
sepmein <sepmein@gmail.com>
2018-01-22 16:25:14 +0800

    redefined training model

--- LSTM.py
+++ LSTM.py
@@ -1,83 +1,126 @@
+import pandas as pd
 import tensorflow as tf
+from oyou import Model
+from twone import RNNContainer
 
-from build_input_fn import rnn_input_fn
+# #############################################################
+#  build input fn
+df = pd.read_csv('interpolated_data_without_date.csv')
+feature_tags = [
+    'month', 'day', 'temp', 'rh', 'so2', 'no2', 'co', 'pm10', 'pm2.5', 'o3', 'dow'
+]
+target_tag = [
+    'death_total'
+]
 
-features, targets, cv_features, cv_targets = rnn_input_fn()
-hidden_size = 1
-batch_size = 1
-max_length = features.shape[1]
-feature_size = features.shape[2]
-print('feature shape:', features.shape)
-print('target shape:', targets.shape)
-# rnn_cell = tf.nn.rnn_cell.GRUCell(hidden_size)
-# create 2 LSTMCells
-rnn_layers = [
-    tf.nn.rnn_cell.GRUCell(size) for size in [hidden_size]]
+container = RNNContainer(
+    data_frame=df
+)
 
-# create a RNN cell composed sequentially of a number of RNNCells
-multi_rnn_cell = tf.nn.rnn_cell.MultiRNNCell(rnn_layers)
+container.set_feature_tags(feature_tags)
+container.set_target_tags(target_tag)
 
-# initial_state = rnn_cell.zero_state(batch_size, dtype=tf.float32)
+container.gen_batch_for_sequence_classification(
+    batch=5,
+    time_steps=100,
+    randomly=True
+)
 
-input_data = tf.placeholder(shape=(batch_size, None, feature_size), dtype=tf.float32)
-t = tf.placeholder(shape=None, dtype=tf.float32)
+# ###############################################################
+# build tensorflow graph
+# placeholders
+features = tf.placeholder(
+    dtype=tf.float32,
+    shape=[container.__batch__, container.__time_steps__, container.num_features],
+    name='features'
+)
+targets = tf.placeholder(
+    dtype=tf.float32,
+    shape=[container.__batch__, 1, container.num_targets]
+)
+# cells
+num_units = 150
+cell = tf.nn.rnn_cell.LSTMCell(
+    num_units=num_units
+)
+output, final_state = tf.nn.dynamic_rnn(
+    cell=cell,
+    inputs=features,
+    dtype=tf.float32
+)
 
-outputs, state = tf.nn.dynamic_rnn(multi_rnn_cell, input_data, dtype=tf.float32)
-print('output shape:', outputs.shape)
-# initial w and b for the network
-unstacked = tf.reshape(outputs, shape=(hidden_size, -1))
-print('unstack shape:', unstacked.shape)
+print('ouput shape:', output.get_shape())
+output_transposed = tf.transpose(
+    output,
+    [1, 0, 2]
+)
 
-w = tf.get_variable(name='w_1',
-                    shape=[128, hidden_size],
-                    dtype=tf.float32,
-                    # regularizer=tf.contrib.layers.l2_regularizer(0.01)
-                    )
-b = tf.get_variable(name='b_1',
-                    shape=[128],
-                    dtype=tf.float32)
-h_1 = tf.matmul(unstacked, w, transpose_a=True, transpose_b=True) + b
-# print('h_1 shape:', h_1.shape)
-a_1 = tf.nn.leaky_relu(h_1)
+time_steps_length_of_output = int(output_transposed.get_shape()[0])
+batches = int(output_transposed.get_shape()[1])
 
-w_2 = tf.get_variable(name='w_2', shape=[64, 128], dtype=tf.float32, )
-b_2 = tf.get_variable(name='b_2', shape=[64], dtype=tf.float32, )
-h_2 = tf.matmul(a_1, w_2, transpose_b=True) + b_2
-a_2 = tf.nn.leaky_relu(h_2)
+# gather the last output
+last_output = tf.gather(
+    output_transposed,
+    time_steps_length_of_output - 1
+)
+last_output_reshaped = tf.reshape(
+    last_output,
+    shape=[-1, num_units]
+)
+# last_output_transposed = tf.transpose(last_output, [1, 0, 2])
+# predictions
+predictions = tf.contrib.layers.fully_connected(
+    last_output_reshaped,
+    container.num_targets
+)
 
-w_3 = tf.get_variable(name='w_3', shape=[32, 64], dtype=tf.float32, )
-b_3 = tf.get_variable(name='b_3', shape=[32], dtype=tf.float32, )
-h_3 = tf.matmul(a_2, w_3, transpose_b=True) + b_3
-a_3 = tf.nn.leaky_relu(h_3)
+predictions_reshaped_for_losses = tf.reshape(
+    predictions,
+    shape=(container.__batch__, 1, container.num_targets)
+)
 
-w_4 = tf.get_variable(name='w_4', shape=[1, 32], dtype=tf.float32)
-b_4 = tf.get_variable(name='b_4', shape=[1], dtype=tf.float32)
-predicted = tf.matmul(a_3, w_4, transpose_b=True) + b_4
-print('predicted shape:', predicted.shape)
+# define losses
+losses = tf.losses.mean_squared_error(
+    labels=targets,
+    predictions=predictions_reshaped_for_losses
+)
 
-loss = tf.reduce_mean(tf.abs(h_3 - t))
-optimizer = tf.train.AdamOptimizer().minimize(loss)
+# optimizer
+optimizer = tf.train.AdamOptimizer()
+train = optimizer.minimize(losses)
 
-session = tf.Session()
-session.run(tf.global_variables_initializer())
-state = session.run(state, feed_dict={input_data: features})
-# print('state shape:', state.shape)
-print(state)
+################################################################
+# use oyou to define the model
+model = Model(name='death_environment')
+model.features = features
+model.targets = targets
+model.prediction = predictions
+model.losses = losses
 
-for i in range(1000000):
-    l, _ = session.run(
-        [loss, optimizer],
-        feed_dict={
-            input_data: features,
-            t: targets
-        }
-    )
-    if i % 10 == 0:
-        l_cv = session.run(loss, feed_dict={
-            input_data: cv_features,
-            t: cv_targets
-        })
-        print('step:', i, 'loss', l, 'cross val loss:', l_cv)
+model.create_log_group(
+    name='training'
+)
+model.create_log_group(
+    name='cv'
+)
 
-        # if i % 100 == 0:
-        # print(session.run(state, feed_dict={input_data: features}))
+model.log_scalar(name='loss',
+                 tensor=losses,
+                 group='training')
+model.log_scalar(name='loss',
+                 tensor=losses,
+                 group='cv')
+# model.define_saving_strategy(indicator_tensor=losses,
+#                              interval=50,
+#                              feed_dict=[features, targets])
+model.train(
+    features=container.get_training_features,
+    targets=container.get_training_targets,
+    training_features=container.get_training_features,
+    training_targets=container.get_training_targets,
+    cv_features=container.get_cv_features,
+    cv_targets=container.get_cv_targets,
+    saving_features=container.get_cv_features,
+    saving_targets=container.get_cv_targets,
+    training_steps=100000
+)
